9500b3ee855586fe1358badfe3c83f72b1d3ceb6
Gonesh Chandra Das <gonu923@gmail.com>
2020-10-23 14:13:53 +0600

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 	$google_client = new Google_Client();
 	$google_client->setClientId('730052532493-jcarl5e7vl503vvb4d2mkemlcbuv6faa.apps.googleusercontent.com');
 	$google_client->setClientSecret('Nlf7WmxTP1PrMasiX_W4HSLP');
-	$google_client->setRedirectUri('https://enewspaper923.herokuapp.com/index.php');
+	$google_client->setRedirectUri('https://enewspaper923.herokuapp.com');
 	$google_client->addScope('email');
 	$google_client->addScope('profile');
 
